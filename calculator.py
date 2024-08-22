696d2068c817f03d6ed2aa6441522917504a6a97
sharongeorge69 <georgesharon6969@gmail.com>
2024-08-22 06:53:46 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,14 @@
-print("Work here")
+print("Welcome to the tip calculator")
+bill = float(input("Enter the amount in the bill :"))
+tip = float(input("How much tip ?10,12 or 15 : "))
+spilt = int(input("How much person do you want to split it with : "))
+percentage = (tip / 100) * bill
+result =(bill+percentage)/spilt
+total_bill=round(result, 2)
+print(f"Each should pay : {total_bill}")
+
+
+
+
+
+
